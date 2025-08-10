30467f32eb3135dd44db8a60bb1507b71c51417f
mmonekyy <ezsabyczku123@gmail.com>
2025-08-10 19:36:17 +0200

    Change post to post_sell
    Change post to post_sell

--- forum_posts_sell/apps.py
+++ forum_posts_sell/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ForumPostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'forum_posts'
+    name = 'forum_posts_sell'
